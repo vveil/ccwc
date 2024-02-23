c8a1b83eb5aac82517ba11636c9aea2b564e945c
Niklas <account@niklas.ai>
2024-02-23 14:56:23 +0100

    implemented reading from standard input

--- Sources/ccwc/main.swift
+++ Sources/ccwc/main.swift
@@ -1,31 +1,75 @@
 import Foundation
 
-func getParamAfterFlag(flag: String, parameters: [String]) -> String? {
-  if let index = parameters.firstIndex(of: flag) {
-    let indexAfterFlag = index + 1
-    if indexAfterFlag
-      < parameters.endIndex
-    {
-      return parameters[indexAfterFlag]
+func fileSizeInBytes(forPath path: String) -> UInt64? {
+  let fileManager = FileManager.default
+
+  do {
+    let attributes = try fileManager.attributesOfItem(atPath: path)
+    if let fileSize = attributes[.size] as? UInt64 {
+      return fileSize
+    } else {
+      print("Could not retrieve file size.")
+      return nil
     }
+  } catch {
+    print("Error: \(error)")
+    return nil
   }
-  return nil
 }
 
-let arguments = CommandLine.arguments
+var arguments = CommandLine.arguments
+var output = ""
 
-if arguments.count > 1 {
+var content = ""
+var path = ""
+if arguments.count == 1 {
+  while let line = readLine() {
+    content.append(line)
+  }
+  if content.isEmpty {
+    print("No input provided.")
+    exit(1)
+  }
+} else {
+  if let filePath = Array(arguments[1...]).filter({ !$0.hasPrefix("-") }).first {
+    path = filePath
+    if let fileContent = try? String(contentsOfFile: path) {
+      content = fileContent
+    } else {
+      exit(1)
+    }
+  } else {
+    exit(1)
+  }
+}
+
+if arguments.count == 2 {
+  // add arguments for default call
+  arguments += ["-l", "-w", "-c"]
+}
+
+if arguments.count > 2 {
   let parameters = Array(arguments[1...])
 
-  if let paramAfterCFlag = getParamAfterFlag(flag: "-c", parameters: parameters) {
-    print("Parameter after -c flag: \(paramAfterCFlag)")
-    
-  } else {
-    print("No parameter after -c flag.")
+  if parameters.contains("-l") {
+    output.append(String(content.components(separatedBy: "\n").count))
   }
 
-  print("Parameter provided: \(parameters)")
+  if parameters.contains("-w") {
+    let words = content.split { $0.isWhitespace }
+    output.append("  \(words.count)")
+  }
 
-} else {
-  print("No parameter provided.")
+  if parameters.contains("-m") {
+    output.append("  \(String(content.count))")
+  }
+
+  if parameters.contains("-c") {
+    if let size = fileSizeInBytes(forPath: path) {
+      output.append("  \(String(size))")
+    }
+
+  }
+  output.append("  \(path)")
 }
+print(output)
