e53337d1953bf45ab344ccd5dc7ab80bea52da0d
Niklas <account@niklas.ai>
2024-02-23 11:32:32 +0100

    removed comment

--- Sources/ccwc/main.swift
+++ Sources/ccwc/main.swift
@@ -5,7 +5,7 @@ let arguments = CommandLine.arguments
 if arguments.count > 1 {
   let parameter = arguments[1]
   print("Parameter provided: \(parameter)")
-  // Implement your tool's logic here
+
 } else {
   print("No parameter provided.")
 }
