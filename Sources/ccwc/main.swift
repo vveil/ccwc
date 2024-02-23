f35d98f4553f9817e025f159cf136fda72ec1516
Niklas <account@niklas.ai>
2024-02-23 11:56:12 +0100

    added function to get parameter after flag

--- Sources/ccwc/main.swift
+++ Sources/ccwc/main.swift
@@ -1,10 +1,30 @@
 import Foundation
 
+func getParamAfterFlag(flag: String, parameters: [String]) -> String? {
+  if let index = parameters.firstIndex(of: flag) {
+    let indexAfterFlag = index + 1
+    if indexAfterFlag
+      < parameters.endIndex
+    {
+      return parameters[indexAfterFlag]
+    }
+  }
+  return nil
+}
+
 let arguments = CommandLine.arguments
 
 if arguments.count > 1 {
-  let parameter = arguments[1]
-  print("Parameter provided: \(parameter)")
+  let parameters = Array(arguments[1...])
+
+  if let paramAfterCFlag = getParamAfterFlag(flag: "-c", parameters: parameters) {
+    print("Parameter after -c flag: \(paramAfterCFlag)")
+    
+  } else {
+    print("No parameter after -c flag.")
+  }
+
+  print("Parameter provided: \(parameters)")
 
 } else {
   print("No parameter provided.")
